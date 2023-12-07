63fcab2675a2e89fe1954e74a871aeed2c91957c
Paninskii <pancration85@mail.ru>
2023-12-07 11:11:53 +0300

    start

---
+++ src/main/java/com/courseModel/courseModel/CourseModelApplication.java
@@ -0,0 +1,13 @@
+package com.courseModel.courseModel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseModelApplication.class, args);
+	}
+
+}
